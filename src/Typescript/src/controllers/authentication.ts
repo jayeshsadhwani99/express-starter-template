60ab7faac37c795ad9567f02fbcf0f54e2023f97
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-01-24 01:00:17 +0530

    Auth using bcrypt

--- src/Typescript/src/controllers/authentication.ts
+++ src/Typescript/src/controllers/authentication.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import ExpressError from "../utils/ExpressError";
 import jwt from "jsonwebtoken";
+import { genSalt, hash, compare } from "bcrypt";
 
 import User, { UserType } from "../models/user";
 
@@ -16,21 +17,25 @@ export const login = async (
 
     const user: UserType = await User.findOne({
       email: email,
-      password: password,
     });
 
     if (user) {
-      const token: string = jwt.sign(
-        {
-          id: user._id,
-        },
-        secret,
-        { expiresIn: 60 * 60 * 24 * 7 }
-      );
-      res.status(200).json({
-        success: true,
-        accessToken: token,
-      });
+      const validPassword = await compare(password, user.password);
+      if (validPassword) {
+        const token: string = jwt.sign(
+          {
+            id: user._id,
+          },
+          secret,
+          { expiresIn: 60 * 60 * 24 * 7 }
+        );
+        res.status(200).json({
+          success: true,
+          accessToken: token,
+        });
+      } else {
+        next(new ExpressError("Invalid credentials", 500));
+      }
     } else {
       next(new ExpressError("Invalid credentials", 500));
     }
@@ -47,14 +52,19 @@ export const signup = async (
   const secret = process.env.JWT_SECRET;
   try {
     const name = req.body.name;
-    const password = req.body.password;
     const email = req.body.email;
+
+    // Hash password
+    const salt = await genSalt(10);
+    const password = await hash(req.body.password, salt);
 
     const user: UserType = new User({
       name,
       email,
       password,
     });
+
+    await user.save();
 
     const token: string = jwt.sign(
       {
